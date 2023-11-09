c696f60ceba69c12ab9803986c79eb6c14189884
ikorotkaya <irinkakorotkaya@gmail.com>
2023-11-09 16:56:01 +0100

    Disable mapTypeControls, add sidebar that opens on marker click, style sidebar above the map

--- src/components/GoogleMap.tsx
+++ src/components/GoogleMap.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { GoogleMap, LoadScript, MarkerF } from "@react-google-maps/api";
 import pin from "../images/pin.svg";
 
 import { GoogleMapsComponentProps } from "../types";
+import Sidebar from "./Sidebar";
 const containerStyle = {
   width: "100%",
   height: "100%",
@@ -13,9 +14,28 @@ const center = {
   lng: 9.9872,
 };
 
+const googleMapOptions = {
+  streetViewControl: false,
+  mapTypeControl: false,
+  fullscreenControl: false,
+  maxZoom: 16,
+  minZoom: 5,
+};
+
 export default function GoogleMapsComponent({
   projects,
 }: GoogleMapsComponentProps) {
+  const [isGoogleMapsLoaded, setIsGoogleMapsLoaded] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const handleMarkerClick = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const handleGoogleMapsLoad = () => {
+    setIsGoogleMapsLoaded(true);
+  };
+
   return (
     <div className="map_container">
       <LoadScript
@@ -25,21 +45,34 @@ export default function GoogleMapsComponent({
             : ""
         }
       >
-        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={11}>
-          {projects && projects.map((project, index) => (
-            <MarkerF
-              key={index}
-              position={{ lat: Number(project.lat), lng: Number(project.lng) }}
-              title={project.title}
-              options={{
-                icon: {
-                  url: pin,
-                  scaledSize: new window.google.maps.Size(15, 15),
-                },
-              }}
-            />
-          ))}
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          options={googleMapOptions}
+
+          onLoad={handleGoogleMapsLoad}
+          zoom={11}
+        >
+          {isGoogleMapsLoaded &&
+            projects.map((project, index) => (
+              <MarkerF
+                key={index}
+                position={{
+                  lat: Number(project.lat),
+                  lng: Number(project.lng),
+                }}
+                title={project.title}
+                options={{
+                  icon: {
+                    url: pin,
+                    scaledSize: new window.google.maps.Size(15, 15),
+                  },
+                }}
+                onClick={handleMarkerClick}
+              />
+            ))}
         </GoogleMap>
+        {isSidebarOpen && <Sidebar projects={projects} />}
       </LoadScript>
     </div>
   );
