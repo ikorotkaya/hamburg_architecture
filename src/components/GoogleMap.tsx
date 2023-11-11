5819810300aef01a58ccba1e02333baef4763f81
ikorotkaya <irinkakorotkaya@gmail.com>
2023-11-11 20:40:55 +0100

    Added supercluster package, added clusterisation to the map, added pin icons

--- src/components/GoogleMap.tsx
+++ src/components/GoogleMap.tsx
@@ -1,13 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import {
   GoogleMap,
   LoadScript,
   MarkerF,
   InfoWindowF,
 } from "@react-google-maps/api";
-import pin from "../images/pin.svg";
+import Supercluster, { ClusterFeature, PointFeature } from "supercluster";
 
-import { GoogleMapsComponentProps } from "../types";
+import pin from "../images/pin.svg";
+import clusterPinIcon from "../images/cluster-pin-icon.svg";
+import pinActiveIcon from "../images/pin-active-icon.svg";
+
+import { GoogleMapsComponentProps, ProjectProps } from "../types";
 import ProjectPopUp from "./ProjectPopUp";
 
 const containerStyle = {
@@ -28,19 +32,122 @@ const googleMapOptions = {
   minZoom: 5,
 };
 
+const MIN_CLUSTER_POINTS = 3;
+
+type Map = google.maps.Map & { zoom: number };
+type ProjectCluster = ClusterFeature<{ project: ProjectProps }> & {
+  properties: { project: ProjectProps };
+};
+
 export default function GoogleMapsComponent({
   projects,
 }: GoogleMapsComponentProps) {
   const [isGoogleMapsLoaded, setIsGoogleMapsLoaded] = useState(false);
-  const [selectedProjectId, setSelectedProjectId] = useState<number | null>(null);
+  const [selectedProjectId, setSelectedProjectId] = useState<number | null>(
+    null
+  );
+
+  const mapRef = useRef<Map>();
+  const [zoom, setZoom] = useState<number>(12);
+  const [bounds, setBounds] = useState<GeoJSON.BBox>([0, 0, 0, 0]);
+  const [clusters, setClusters] = useState<ProjectCluster[]>([]);
+  const [supercluster, setSupercluster] = useState<Supercluster>(
+    new Supercluster({
+      radius: 75,
+      maxZoom: googleMapOptions.maxZoom,
+      minPoints: MIN_CLUSTER_POINTS,
+    })
+  );
 
   const handleMarkerClick = (projectId: number | null) => {
     setSelectedProjectId(projectId);
   };
 
-  const handleGoogleMapsLoad = () => {
+  const handleGoogleMapsLoad = (map: google.maps.Map) => {
     setIsGoogleMapsLoaded(true);
+    mapRef.current = map as Map;
   };
+
+  const handleClusterClick = ({
+    id,
+    lat,
+    lng,
+  }: {
+    id: number;
+    lat: number;
+    lng: number;
+  }) => {
+    const expansionZoom = Math.min(
+      supercluster.getClusterExpansionZoom(id),
+      20
+    );
+    mapRef.current?.setZoom(expansionZoom);
+    mapRef.current?.panTo({ lat, lng });
+  };
+
+  const getLabel = (pointCount: number): google.maps.MarkerLabel => {
+    return {
+      text: pointCount.toString(),
+      color: "#FFF",
+      fontWeight: "bold",
+    };
+  };
+
+  const handleBoundsChanged = () => {
+    if (mapRef.current) {
+      const bounds = mapRef.current.getBounds()?.toJSON();
+
+      setBounds([
+        bounds?.west || 0, // eslint-disable-line @typescript-eslint/strict-boolean-expressions
+        bounds?.south || 0, // eslint-disable-line @typescript-eslint/strict-boolean-expressions
+        bounds?.east || 0, // eslint-disable-line @typescript-eslint/strict-boolean-expressions
+        bounds?.north || 0, // eslint-disable-line @typescript-eslint/strict-boolean-expressions
+      ]);
+    }
+  };
+
+  const handleZoomChanged = () => {
+    if (mapRef.current) {
+      setZoom(mapRef.current?.zoom);
+    }
+  };
+
+  const formatDataToGeoJsonPoints = (
+    projects: ProjectProps[]
+  ): GeoJSON.Feature<GeoJSON.Point>[] => {
+    return projects.map((project) => ({
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [project.lng, project.lat],
+      },
+      properties: { cluster: false, project },
+    }));
+  };
+
+  useEffect(() => {
+    const radius = (100 * googleMapOptions.maxZoom) / zoom;
+
+    setSupercluster(
+      new Supercluster({
+        radius: radius,
+        maxZoom: googleMapOptions.maxZoom,
+        minPoints: MIN_CLUSTER_POINTS,
+      })
+    );
+  }, [zoom]);
+
+  useEffect(() => {
+    if (mapRef.current) {
+      supercluster.load(
+        formatDataToGeoJsonPoints(projects) as PointFeature<
+          GeoJSON.Feature<GeoJSON.Point>
+        >[]
+      );
+
+      setClusters(supercluster.getClusters(bounds, zoom) as ProjectCluster[]);
+    }
+  }, [projects, bounds, zoom]);
 
   return (
     <div className="map_container">
@@ -54,41 +161,78 @@ export default function GoogleMapsComponent({
         <GoogleMap
           mapContainerStyle={containerStyle}
           center={center}
+          onBoundsChanged={handleBoundsChanged}
+          onZoomChanged={handleZoomChanged}
           options={googleMapOptions}
           onLoad={handleGoogleMapsLoad}
-          zoom={11}
+          zoom={zoom}
         >
           {isGoogleMapsLoaded &&
-            projects.map((project, index) => (
-              <MarkerF
-                key={index}
-                position={{
-                  lat: Number(project.lat),
-                  lng: Number(project.lng),
-                }}
-                title={project.title}
-                options={{
-                  icon: {
-                    url: pin,
-                    scaledSize: new window.google.maps.Size(15, 15),
-                  },
-                }}
-                onClick={() => handleMarkerClick(project.id)}
-              >
-                {selectedProjectId === project.id && (
-                  <InfoWindowF
-                    position={{
-                      lat: Number(project.lat),
-                      lng: Number(project.lng),
-                    }}
-                    onCloseClick={() => handleMarkerClick(null)}
-                    options={{ disableAutoPan: false }}
-                  >
-                    <ProjectPopUp project={project} />
-                  </InfoWindowF>
-                )}
-              </MarkerF>
-            ))}
+            clusters.map(({ id, geometry, properties }) => {
+              const [lng, lat] = geometry.coordinates;
+              const { cluster, point_count } = properties;
+
+              if (selectedProjectId !== null && cluster === true) {
+                return null;
+              }
+              if (
+                selectedProjectId !== null &&
+                selectedProjectId !== properties.project.id
+              ) {
+                return null;
+              }
+              return cluster ? (
+                <MarkerF
+                  key={`cluster-${id}`}
+                  onClick={() =>
+                    handleClusterClick({ id: id as number, lat, lng })
+                  }
+                  position={{ lat, lng }}
+                  options={{
+                    icon: {
+                      url: clusterPinIcon,
+                      scaledSize: new window.google.maps.Size(48, 48),
+                    },
+                  }}
+                  label={getLabel(point_count)}
+                />
+              ) : (
+                <MarkerF
+                  key={properties.project.id}
+                  position={{
+                    lat: Number(properties.project.lat),
+                    lng: Number(properties.project.lng),
+                  }}
+                  title={properties.project.title}
+                  options={{
+                    icon: {
+                      url:
+                        selectedProjectId === properties.project.id
+                          ? pinActiveIcon
+                          : pin,
+                      scaledSize:
+                        selectedProjectId === properties.project.id
+                          ? new window.google.maps.Size(60, 60)
+                          : new window.google.maps.Size(30, 30),
+                    },
+                  }}
+                  onClick={() => handleMarkerClick(properties.project.id)}
+                >
+                  {selectedProjectId === properties.project.id && (
+                    <InfoWindowF
+                      position={{
+                        lat: Number(properties.project.lat),
+                        lng: Number(properties.project.lng),
+                      }}
+                      onCloseClick={() => handleMarkerClick(null)}
+                      options={{ disableAutoPan: false }}
+                    >
+                      <ProjectPopUp project={properties.project} />
+                    </InfoWindowF>
+                  )}
+                </MarkerF>
+              );
+            })}
         </GoogleMap>
       </LoadScript>
     </div>
