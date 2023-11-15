52bf1ef4d7bfa8020ff04cde1483e5cbd0d3146e
ikorotkaya <irinkakorotkaya@gmail.com>
2023-11-15 14:17:42 +0100

    Change title language dinamically

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import logo from "../logo.svg";
 import LanguageSwitcher from "./LanguageSwitcher";
+import { useTranslation } from "react-i18next";
 
 export default function Header() {
+  const { t } = useTranslation();
   return (
     <div className="header_container">
       <div className="header_container__logo">
         <img src={logo} alt="logo" />
       </div>
       <p className="header_container__title">
-        Hamburg Architecture
+      {t("title")}
       </p>
       <LanguageSwitcher  />
     </div>
